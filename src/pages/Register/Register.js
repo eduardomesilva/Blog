7103bbe80663284203130e0e21b3b84775835933
Eduardo Messias <messiasgit@outlook.com>
2024-05-01 15:20:39 -0300

    feat: Paginas Login e Register, css global da pagina, rotas para novas paginas

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,34 @@
+import style from './Register.module.css'
+
+import {useState, useEffect} from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <h1>Cadastre-se para postar</h1>
+        <p>Crie uma conta para compartinhas suas histórias!</p>
+        <form>
+            <label>
+                <span>Nome:</span>
+                <input type='text' name='displayname' required placeholder='Nome do usuário'/>
+            </label>
+            <label>
+                <span>E-mail:</span>
+                <input type='email' name='email' required placeholder='E-mail do usuário'/>
+            </label>
+            <label>
+                <span>Senha:</span>
+                <input type='password' name='password' required placeholder='Insira sua senha'/>
+            </label>
+            <label>
+                <span>Confirmação de senha:</span>
+                <input type='password' name='ConfirmPassword' required placeholder='Confime a  sua senha'/>
+            </label>
+            <button className='btn'>Cadastrar</button>
+        </form>
+        
+    </div>
+  )
+}
+
+export default Register
