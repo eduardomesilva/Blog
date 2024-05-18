990a8dbf24c049122c8d02065d015343bc2483a3
Eduardo Messias <messiasgit@outlook.com>
2024-05-18 10:58:03 -0300

    colocando usuario no contexto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth';
+
+//hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
 
 //context
 import { AuthProvider } from './context/AuthContext';
@@ -15,11 +20,27 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  const loadindgUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if(loadindgUser){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-     <AuthProvider>
+     <AuthProvider value={{user}}>
       <BrowserRouter>
-        <Navbar />
+      <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
